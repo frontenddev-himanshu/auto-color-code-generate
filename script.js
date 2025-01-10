ff0e7b016f4035fa208f4260af294f4fbca64d53
frontenddev-himanshu <rajh5343@gmail.com>
2025-01-10 12:20:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let on = document.getElementById("onBtn");
+let off = document.getElementById("offBtn");
+let body = document.querySelector("body");
+
+function randomColor() {
+
+    let colorRange = "0123456789ABCDEF";
+
+    let colorCode = "#";
+
+    for (let index = 0; index < 6; index++) {
+
+        colorCode += colorRange[Math.floor(Math.random() * 16)]
+    }
+
+    return colorCode
+
+}
+
+
+let auto;
+
+on.addEventListener("click", () => {
+
+    auto = setInterval(() => {
+
+        body.style.backgroundColor = randomColor();
+
+    }, 1000);
+
+})
+
+
+
+off.addEventListener("click", () => {
+
+    clearInterval(auto);
+
+})
